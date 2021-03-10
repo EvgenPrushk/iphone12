aa832799bd07b23b266e4577e3462dec850dea43
EvgenPrushk <ogmprushak@mail.ru>
2021-03-10 20:55:42 +0300

    feat: add DOM

--- script/script.js
+++ script/script.js
@@ -1,7 +1,55 @@
 document.addEventListener("DOMContentLoaded", () => {
   "use strict";
   const tabs = () => {
-      const cardDetailChangeElems = document.querySelectorAll('')
+    const cardDetailChangeElems = document.querySelectorAll(
+      ".card-detail__change"
+    );
+    const cardDetailsTitleElem = document.querySelector(".card-details__title");
+    const cardImageItemElem = document.querySelector(".card__image_item");
+    const cardDetailsPriceElem = document.querySelector(".card-details__price");
+    const descriptionMemory = document.querySelector(".description__memory");
+    // массив с данными телефонов
+    const data = [
+      {
+        name: "Смартфон Apple iPhone 12 Pro 128GB Graphite",
+        img: "img/iPhone-graphite.png",
+        price: "95990",
+        memoryROM: 128,
+      },
+      {
+        name: "Смартфон Apple iPhone 12 Pro 256GB Silver",
+        img: "img/iPhone-silver.png",
+        price: "120990",
+        memoryROM: 256,
+      },
+      {
+        name: "Смартфон Apple iPhone 12 Pro 128GB Pacific Blue",
+        img: "img/iPhone-blue.png",
+        price: "99999",
+        memoryROM: 128,
+      },
+    ];
+
+    const deactive = () => {
+      // forEach 
+      cardDetailChangeElems.forEach((btn) => btn.classList.remove("active"));
+    };
+    // перебор массива с помощью функции 
+    cardDetailChangeElems.forEach((btn, i) => {
+      btn.addEventListener("click", () => {
+        // проверяем есть ли у элемента класс active
+        if (!btn.classList.contains("active")) {
+          deactive();
+          btn.classList.add("active");
+          cardDetailsTitleElem.textContent = data[i].name;
+          cardImageItemElem.src = data[i].img;
+          cardImageItemElem.alt = data[i].name;
+          cardDetailsPriceElem.textContent = data[i].price + "₽";
+          descriptionMemory.textContent = `Встроенная память (ROM) ${data[i].memoryROM}
+          ГБ`;
+        }
+      });
+    });
   };
 
   tabs();
